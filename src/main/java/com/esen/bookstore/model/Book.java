a981636d6837ecc5af66a8c79e840d275ae55560
Joo Szebasztian <jooszebi26@gmail.com>
2025-04-22 09:36:45 +0200

    Added default constructor to Book
    Signed-off-by: Joo Szebasztian <jooszebi26@gmail.com>

--- src/main/java/com/esen/bookstore/model/Book.java
+++ src/main/java/com/esen/bookstore/model/Book.java
@@ -4,13 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 
     @Id

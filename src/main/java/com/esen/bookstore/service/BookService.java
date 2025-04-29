7e0668028563e0412fce25fe2d7013014941153b
Joo Szebasztian <jooszebi26@gmail.com>
2025-04-29 08:41:11 +0200

    Added delete and update methods to BookService.java
    Signed-off-by: Joo Szebasztian <jooszebi26@gmail.com>

--- src/main/java/com/esen/bookstore/service/BookService.java
+++ src/main/java/com/esen/bookstore/service/BookService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +21,42 @@ public class BookService {
 
     public List<Book> findAll() {
         return booksRepository.findAll();
+    }
+
+    public void delete(Long id) {
+        if (!booksRepository.existsById(id)) {
+            throw new IllegalArgumentException("Cannot find book with id " + id);
+        }
+
+        booksRepository.deleteById(id);
+    }
+
+    public Book update(Long id, String title, String author, String publisher, Double price) {
+        if (Stream.of(title, author, publisher, price).allMatch(Objects::isNull)) {
+            throw new IllegalArgumentException("At leas one input is required");
+        }
+
+        if (!booksRepository.existsById(id)) {
+            throw new IllegalArgumentException("Cannot find book with id " + id);
+        }
+
+        var book = booksRepository.findById(id).get();
+
+        if (title != null) {
+            book.setTitle(title);
+        }
+
+        if (author != null) {
+            book.setAuthor(author);
+        }
+
+        if (publisher != null) {
+            book.setPublisher(publisher);
+        }
+
+        if (price != null) {
+            book.setPrice(price);
+        }
+        return book;
     }
 }
